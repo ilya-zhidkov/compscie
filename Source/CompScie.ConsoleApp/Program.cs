eb7146bc99104b4ec3cafd11048f7396093ff2f3
ilya-zhidkov <zidkov.ilja@gmail.com>
2020-06-23 10:05:02 +0200

    Insert a node into a Binary Tree

--- Source/CompScie.ConsoleApp/Program.cs
+++ Source/CompScie.ConsoleApp/Program.cs
@@ -1,9 +1,19 @@
-﻿using CompScie.ConsoleApp.Demos.SearchDemo;
+﻿using CompScie.Core;
 
 namespace CompScie.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args) => SearchDemo.Show();
+        static void Main(string[] args)
+        {
+            var tree = new Tree();
+            tree.Insert(7);
+            tree.Insert(4);
+            tree.Insert(9);
+            tree.Insert(1);
+            tree.Insert(6);
+            tree.Insert(8);
+            tree.Insert(10);
+        }
     }
 }
---
+++ Source/CompScie.Core/Tree.cs
@@ -0,0 +1,55 @@
+﻿namespace CompScie.Core
+{
+    public class Tree
+    {
+        private class Node
+        {
+            public int Value { get; set; }
+            public Node LeftChild { get; set; }
+            public Node RightChild { get; set; }
+
+            public Node(int value) => Value = value;
+
+            public override string ToString() => $"Node = {Value}";
+        }
+
+        private Node root;
+
+        public void Insert(int value)
+        {
+            var node = new Node(value);
+
+            if (root == null)
+            {
+                root = node;
+                return;
+            }
+
+            var current = root;
+
+            while (true)
+            {
+                if (value < current.Value)
+                {
+                    if (current.LeftChild == null)
+                    {
+                        current.LeftChild = node;
+                        break;
+                    }
+
+                    current = current.LeftChild;
+                }
+                else
+                {
+                    if (current.RightChild == null)
+                    {
+                        current.RightChild = node;
+                        break;
+                    }
+
+                    current = current.RightChild;
+                }
+            }
+        }
+    }
+}
